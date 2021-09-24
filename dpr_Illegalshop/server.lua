83261b83c3471b4e8439420757286aff791d9d21
Desperados <89983547+Desperados-Creation@users.noreply.github.com>
2021-09-24 20:20:09 +0200

    Update server.lua

--- dpr_Illegalshop/server.lua
+++ dpr_Illegalshop/server.lua
@@ -4,15 +4,12 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 RegisterNetEvent('dpr_Illegalshop:AchatItem')
 AddEventHandler('dpr_Illegalshop:AchatItem', function(Nom, Item, Price)
+    local _src = source
     local xPlayer = ESX.GetPlayerFromId(source)
     if xPlayer.getMoney() >= Price then 
         xPlayer.removeMoney(Price)
         xPlayer.addInventoryItem(Item, 1)
-        local xPlayers = ESX.GetPlayers()
-        for i=1, #xPlayer, 1 do 
-            local xPlayer = ESX.GetPlayerFromId(xPlayer[i])
-            Citizen.Wait(500)
-            TriggerClientEvent('esx:showAdvancedNotification', xPlayers[i], 'Supérette', '~o~2~r~4~g~7', "Vous venez d\'acheter ~b~1x "..Nom.." ~s~pour ~r~"..Price.."$ ~s~!", 'CHAR_ACTING_UP', 1)
-        end
+        Citizen.Wait(500)
+        TriggerClientEvent('esx:showAdvancedNotification', _src, 'Supérette', '~o~2~r~4~g~7', "Vous venez d\'acheter ~b~1x "..Nom.." ~s~pour ~r~"..Price.."$ ~s~!", 'CHAR_ACTING_UP', 1)
     end
 end)
